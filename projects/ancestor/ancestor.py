a71d374cee314e90849d65496d008d01d8d4dbc3
Poenomaly <djbenson741@gmail.com>
2020-09-09 14:53:31 -0600

    Added comments to ancestors module for better understanding of code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,6 +4,7 @@ from util import Stack
 def earliest_ancestor(ancestors, starting_node):
     # set up empty graph
     g = Graph()
+    # Create list for possible ancestors
     possible_ancestors = []
 
     # add 11 vertices to the graph
@@ -12,30 +13,51 @@ def earliest_ancestor(ancestors, starting_node):
 
     # Add the edges
     for ancestor in ancestors:
+        # ancestor at position 0 for first vertex 
+        # and position 1 for the connecting vertex
         g.add_edge(ancestor[0], ancestor[1])
+    # Create a list of starting vertices to iterate through
     starting_nodes = [10, 2, 4, 11]
+    # iterate through each starting node
     for id in starting_nodes:
+        # create tempty stack
         path = Stack()
+        # add vertex id to path stack
         path.push([id])
+        # create empty set for visited vertices
         visited = set()
 
+        # while path stack isn't empty
         while path.size() > 0:
+            # pop vertex path from path stack
             vert = path.pop()
+            # check if last vertex in path is in visited
             if vert[-1] not in visited:
+                # check if vertex is the vertex we want
                 if vert[-1] == starting_node:
+                    # append vertex into list of possible ancestors
                     possible_ancestors.append(vert[0])
                 else:
+                    # add vertex to visited list
                     visited.add(vert[-1])
+                    # iterate through vertex's neighbors
                     for n in g.get_neighbors(vert[-1]):
+                        # create list for second path
                         second_path = list(vert)
+                        # append neighbor to second path
                         second_path.append(n)
+                        # push second path into path stack
                         path.push(second_path)
+    # return -1 if ancestor is equal to starting node (no ancestor)
     if min(possible_ancestors) == starting_node:
         return -1
+    # return 10 if 10 is in possible_ancestors list
     elif 10 in possible_ancestors:
         return 10
+    # return the minimum of the possible ancestors list if list isn't empty
     elif len(possible_ancestors) > 0:
         return min(possible_ancestors)
+    # return -1 for any other scenario (ex; starting node not found in graph)
     else:
         return -1
     
